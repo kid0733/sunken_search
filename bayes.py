3ec5fd10d1b3b7c08e97dadc472a50455ffcfe6b
Siddhant Singh Karki <siddhantskarki@gmail.com>
2023-11-17 11:28:52 +1100

    sailor_final_location func

--- bayes.py
+++ bayes.py
@@ -52,7 +52,7 @@ class Search():
         self.sep1=0
         self.sep2=0
         self.sep3=0
-
+    #Drawing Game Map
     def draw_map(self,last_known):
         '''Display basemap with: scale | last known (x,y) location | search areas'''
         cv.line(self.img(20,370),(70,370), (0, 0, 0), 2)
@@ -78,3 +78,34 @@ class Search():
         cv.imshow('Search Area', self.img)
         cv.moveWindow('Search Area', 750, 10)
         cv.waitKey(500)
+    
+    def sailor_final_location(self, num_search_areas):
+        """Return the actual x,y location of the missing person"""
+        #Find sailor coordinates with respect to any search area subarray
+        #Find the random generated x value [upper_limit=self.sa1.shape[1]] 
+        self.sailor_actual[0]=np.random.choice(self.sa1.shape[1],1)
+        #Find the random generated y value [upper_limit=self.sa1.shape[0]] 
+        self.sailor_actual[1]=np.random.choice(self.sa1.shape[0],1)
+
+        #We are using triangular distribution to return the location of the sailor
+        #since we dont actually know the real-life distribution of thsi scenario
+        area=int(random.triangular(1,num_search_areas+1))
+
+        #if area generated = 1
+        if area==1:
+            x=self.sailor_actual[0]+ SA1_CORNERS[0]
+            y=self.sailor_actual[1]+ SA1_CORNERS[1]
+            self.area_actual=1
+        
+        #if area generated = 2
+        elif area==2:
+            x=self.sailor_actual[0]+ SA2_CORNERS[0]
+            y=self.sailor_actual[1]+ SA2_CORNERS[1]
+            self.area_actual=2
+        elif area==3:
+            x=self.sailor_actual[0]+ SA3_CORNERS[0]
+            y=self.sailor_actual[1]+ SA3_CORNERS[1]
+            self.area_actual=3
+
+        return x,y
+
